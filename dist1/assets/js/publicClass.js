8990a1f5d1a164de1d53d7a67fb5edf776e02fde
Octopus <80883080@qq.com>
2017-03-08 18:12:22 +0800

    clear无用文件

--- dist1/assets/js/publicClass.js
+++
@@ -1,2 +0,0 @@
-var $at={};$at.getJson=function(t,e,n){$.getJSON(t,e,function(t){n(t)})},$at.GetDomId=function(t){var e=document.getElementById(t);return e},$at.staticColors=["#265497","#1e87b5","#b7ab64","#bf721c","#c93e3e","#b5b5b5"];
-//# sourceMappingURL=maps/publicClass.js.map
